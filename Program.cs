8b89ce5796d604b5ef7dfafa312b88c7fa1299ef
ProgMahabub21 <85748757+ProgMahabub21@users.noreply.github.com>
2021-06-20 20:44:20 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaggedArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] section = new int[3][];
+            section[0] = new int[2];
+            section[1] = new int[3]; 
+            section[2] = new int[1];
+            int i =0,r;
+            while(i<3)
+            {
+                r = 0;
+                while(r<section[i].Length)
+                {
+                   int marks= Convert.ToInt32(Console.ReadLine());
+                    if (marks > 100)
+                    {
+                        Console.WriteLine("Marks Cant be over 100");
+                        marks = Convert.ToInt32(Console.ReadLine());
+                        section[i][r] = marks;
+                    }
+                    else
+                        section[i][r] = marks;
+                   r++;
+                }
+                i++;  
+            }
+            Result obj1 = new Result(section);
+             //  Result object1 = new Result(section);
+        }
+    }
+}
---
+++ Result.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaggedArray
+{
+    class Result
+    {
+        public Result(int[][] section)
+        {
+            int sum, totalsum = 0;
+            int i = 0, k;
+            float avg ;
+            while(i <3)
+            {
+                k = 0;
+                sum = 0;
+                while(k< section[i].Length)
+                {
+                    sum += section[i][k];
+                    k++;
+                }
+                totalsum += sum;
+                Console.Write("Student Serial {0}", i);
+                Console.WriteLine(" Total marks {0}",sum);
+                i++;
+            }
+            Console.WriteLine("Total Marks of all Students {0}", totalsum);
+            avg = (totalsum / 6);
+            Console.WriteLine("Avg marks of students each course for the section {0}",avg );
+        }
+    }
+}
